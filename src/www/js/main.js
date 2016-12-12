894d2c1dcb4f58ff9e2f0356439962bd00dd9b13
Michael Woode <info@woodesoft.com>
2016-12-12 11:49:56 +0100

    new function for showing current players turn added new class for table view, to show current player

--- src/www/js/main.js
+++ src/www/js/main.js
@@ -49,6 +49,36 @@ function addWinnerPopupTemplate(){
     includeHtml("templates/win-popup.html", "body");
 }
 
+
+function setCurrentPlayer(playerNo) {
+	// changes column bg color for active player
+
+	var ths = $('.gamecard th');
+	var tds = $('.gamecard td');
+	console.log(ths);
+	console.log(tds);
+
+	for (var i = 0; i < ths.length; i++) {
+		// console.log('i = ', i, ths[i]);
+		if ( i%5 == playerNo) {
+			$(ths[i]).addClass('active-player');
+		} else {
+			$(ths[i]).removeClass('active-player');
+		}
+	}
+	for (var i = 0; i < tds.length; i++) {
+		// console.log('i = ', i, tds[i]);
+		if ( i%5 == playerNo) {
+			$(tds[i]).addClass('active-player');
+		} else {
+			$(tds[i]).removeClass('active-player');
+		}
+	}
+	// var allCols = ths.concat(tds);
+	// console.log(allCols);
+
+}
+
 function showWinner(players) {
 
 	if (!players){
