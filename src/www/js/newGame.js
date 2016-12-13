28a682f1f7ca7a1f6ee312240377f15897712f23
Kristoffer Annerfeldt <koffiman@me.com>
2016-12-13 13:01:52 +0100

    minor changes to new game button.

--- src/www/js/newGame.js
+++ src/www/js/newGame.js
@@ -2,14 +2,23 @@ $(document).ready(function(){
 	$("body").on("click",".new-game-button",function(){
 		$("#gamearea").remove();
 		includeHtml("templates/startpage.html","body");
+		
+		
+
+
+
+
 	});
 
-});
+});w
 
 $(document).ready(function(){
 	$("body").on("click", ".start-game-button", function(){
            $("#start-page").remove();
            includeGameArea();
+
+
 	});
 
 });
+
